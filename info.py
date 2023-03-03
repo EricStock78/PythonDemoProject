f1be85450eec2e58ef59a9755bb9944af981870a
ericdstock <eric.stock@otherocean.com>
2023-03-03 09:39:58 -0330

    Added a new module to interact with the user

---
+++ info.py
@@ -0,0 +1,10 @@
+
+
+def get_info():
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    name = input("Enter your name")
+    fav_food = input("Enter your favourite food")
+    activity = input ("Enter your favourite activity")
+
+    print(f"Your name is {name}, your favourite food is {fav_food} and your activity is {activity}")
--- main.py
+++ main.py
@@ -1,20 +1,19 @@
+import info
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def get_info():
-    # Use a breakpoint in the code line below to debug your script.
-    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    name = input("Enter your name")
-    fav_food = input("Enter your favourite food")
-    activity = input ("Enter your favourite activity")
 
-    print(f"Your name is {name}, your favourite food is {fav_food} and your activity is {activity}")
+
+
+def main():
+    info.get_info()
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    get_info()
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
