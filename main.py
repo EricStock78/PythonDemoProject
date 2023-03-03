806ea99149a80a3affec0b20ac21c9f48957f82f
ericdstock <eric.stock@otherocean.com>
2023-03-03 09:21:31 -0330

    First commit!  Added main python file and gitignore

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def get_info():
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    name = input("Enter your name")
+    fav_food = input("Enter your favourite food")
+    activity = input ("Enter your favourite activity")
+
+    print(f"Your name is {name}, your favourite food is {fav_food} and your activity is {activity}")
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_info()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
